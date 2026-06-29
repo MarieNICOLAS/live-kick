626499fa886b41871cd2db8e319e74a5e871b253
Myo <marie.nicolas29@hotmail.com>
2026-06-29 12:09:47 +0200

    test: isolate team statistics repository test

--- backend/src/test/java/com/livekick/repository/TeamStatisticsRepositoryTests.java
+++ backend/src/test/java/com/livekick/repository/TeamStatisticsRepositoryTests.java
@@ -6,6 +6,7 @@ import com.livekick.dto.football.TeamSummaryDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class TeamStatisticsRepositoryTests {
 
     @Autowired
